268494adb0035bf52e68431ed48e2922c2fac64e
JoelQuia <oliver.joel2020@gmail.com>
2024-10-22 17:03:27 -0300

    Add files via upload

---
+++ Array/src/main/java/UsandoArray.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Cristhian
+ */
+public class UsandoArray {
+    public static void main(String[] args){
+        int[] numeros ={10,20,30,40,50,60,70,80,90,100};
+        
+        System.out.println ("Todos os elementos:");
+        for (int numero:numeros){
+            System.out.println("Elemento:" +numero);
+        }
+    }
+    
+}
